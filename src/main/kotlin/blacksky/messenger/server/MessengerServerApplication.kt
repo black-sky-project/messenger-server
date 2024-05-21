d557d0cda515a4512d530046d8450e5591561b0e
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-05-21 17:32:03 +0700

    Initial commit
    Create Spring Boot web server template

---
+++ src/main/kotlin/blacksky/messenger/server/MessengerServerApplication.kt
@@ -0,0 +1,11 @@
+package blacksky.messenger.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MessengerServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<MessengerServerApplication>(*args)
+}
---
+++ src/test/kotlin/blacksky/messenger/server/MessengerServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package blacksky.messenger.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessengerServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
